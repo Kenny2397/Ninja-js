c740645e68c6758efd8f0c7e0f41475daaffb698
Kenny Julián Luque Ticona <kenny.luque.t@uni.pe>
2022-04-26 12:41:26 -0500

    [TAREA] sensei

--- Ninja.js
+++ Ninja.js
@@ -8,13 +8,32 @@ class Ninja{
     sayName = () => console.log(this.name);
     
     showStats() {
-        console.log(this.name);
-        console.log(this.fuerza);
-        console.log(this.velocidad);
-        console.log(this.salud);
+        console.log("name:"+this.name);
+        console.log("fuerza:"+this.fuerza);
+        console.log("velocidad:"+this.velocidad);
+        console.log("salud:"+this.salud);
     }
-    drinkSake = () => this.salud += 10;
+    // drinkSake = () => this.salud += 10;
+    drinkSake() {
+        this.salud += 10;
+    }
 }
+
+class Sensei extends Ninja{
+    constructor(name) {
+        super(name);
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
+    }
+    speakWisdom() {
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+}
+
+
 const ninja1 = new Ninja("Hyabusa",100);
 ninja1.sayName();
 console.log("==============");
@@ -22,3 +41,8 @@ ninja1.showStats();
 console.log("==============");
 ninja1.drinkSake();
 ninja1.showStats();
+console.log("----------------");
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+console.log("==============");
+superSensei.showStats();
